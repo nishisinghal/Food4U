a4c503ae278ae17773c1de99827765a2ac971c3e
Naman1501 <147094593+Nmn1501@users.noreply.github.com>
2025-02-25 23:53:17 +0530

    push

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -3,7 +3,7 @@ const HeroSection=()=>{
     return(
         <div>
             <div className="w-11/12 bg-black m-auto  shadow-2xl shadow-amber-100 rounded-lg ">
-            <img className="h-full w-full object-cover rounded-lg " src="./src/assets/T2.png" alt="" />
+            <img className="h-full w-full object-cover rounded-lg " src="/src/assets/T2.png" alt="" />
             </div>
         </div>
     )
