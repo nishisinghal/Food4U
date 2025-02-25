71d32adb747b5baa6685218f1d53fce61311d15b
Naman1501 <147094593+Nmn1501@users.noreply.github.com>
2025-02-26 00:18:20 +0530

    naman

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -3,7 +3,7 @@ const HeroSection=()=>{
     return(
         <div>
             <div className="w-11/12 bg-black m-auto  shadow-2xl shadow-amber-100 rounded-lg ">
-            <img className="h-full w-full object-cover rounded-lg " src="/src/assets/T2.png" alt="" />
+            <img className="h-full w-full object-cover rounded-lg " src="https://i.pinimg.com/736x/7f/c3/84/7fc384d28bc1af7405042ba454aa03df.jpg" alt="" />
             </div>
         </div>
     )
